919f1c277ed3d02b5e9abd11a3a0d36aba4d7955
Axel Obscura <axosar@gmail.com>
2020-05-20 12:14:03 -0500

    Deploy

---
+++ build/precache-manifest.fd5d5cd117cc5520a492b01a5d367420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137f1c47e878573144d5a2bd46a1be82",
+    "url": "/video/index.html"
+  },
+  {
+    "revision": "ae13c461e7dfa09c819e",
+    "url": "/video/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14259bc8737413494a98",
+    "url": "/video/static/js/2.d5b908ab.chunk.js"
+  },
+  {
+    "revision": "ae13c461e7dfa09c819e",
+    "url": "/video/static/js/main.d93649ea.chunk.js"
+  },
+  {
+    "revision": "3426b82bb1ab4aba9242",
+    "url": "/video/static/js/runtime-main.908ba1d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video/static/media/logo.5d5d9eef.svg"
+  }
+]);
